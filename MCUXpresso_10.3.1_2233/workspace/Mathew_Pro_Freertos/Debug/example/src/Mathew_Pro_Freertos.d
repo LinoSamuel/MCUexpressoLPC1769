2aa6ef3af30d9c29a3d32d0efff4655c5b882901
LinoSamuel <lino.msamuel@gmail.com>
2019-03-21 14:01:38 -0400

    Lino

---
+++ MCUXpresso_10.3.1_2233/workspace/Mathew_Pro_Freertos/Debug/example/src/Mathew_Pro_Freertos.d
@@ -0,0 +1,152 @@
+example/src/Mathew_Pro_Freertos.o example/src/Mathew_Pro_Freertos.d: \
+ ../example/src/Mathew_Pro_Freertos.c \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/board.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/chip.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/lpc_types.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/sys_config.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/cmsis.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/cmsis_175x_6x.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/core_cm3.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/core_cmInstr.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/core_cmFunc.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/chip_lpc175x_6x.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/sysctl_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/clock_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/iocon_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/adc_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/can_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/dac_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/enet_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/gpdma_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/gpio_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/gpioint_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/i2c_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/i2s_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/mcpwm_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/pmu_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/qei_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/ritimer_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/rtc_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/spi_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/ssp_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/timer_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/uart_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/ring_buffer.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/usb_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/wwdt_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/fmc_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/romapi_17xx_40xx.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/iap.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/error.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/board_api.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/lpc_types.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/lpc_phy.h \
+ /home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/board.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/FreeRTOS.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/projdefs.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/example/inc/FreeRTOSConfig.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/portable.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/portmacro.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/mpu_wrappers.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/task.h \
+ /home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/list.h
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/board.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/chip.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/lpc_types.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/sys_config.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/cmsis.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/cmsis_175x_6x.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/core_cm3.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/core_cmInstr.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/core_cmFunc.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/chip_lpc175x_6x.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/sysctl_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/clock_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/iocon_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/adc_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/can_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/dac_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/enet_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/gpdma_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/gpio_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/gpioint_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/i2c_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/i2s_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/mcpwm_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/pmu_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/qei_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/ritimer_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/rtc_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/spi_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/ssp_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/timer_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/uart_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/ring_buffer.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/usb_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/wwdt_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/fmc_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/romapi_17xx_40xx.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/iap.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/error.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/board_api.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_chip_175x_6x/inc/lpc_types.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/lpc_phy.h:
+
+/home/mathew/git/MCUexpressoLPC1769/MCUXpresso_10.3.1_2233/workspace/lpc_board_nxp_lpcxpresso_1769/inc/board.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/FreeRTOS.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/projdefs.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/example/inc/FreeRTOSConfig.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/portable.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/portmacro.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/mpu_wrappers.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/task.h:
+
+/home/mathew/git/MCUexpressoLPC1769/Mathew_Pro_Freertos/freertos/inc/list.h:
